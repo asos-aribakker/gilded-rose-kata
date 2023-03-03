cf631169bc445473d5fb6e803db4463bb2e89d0a
Ari Bakker <ari.bakker@asos.com>
2023-03-03 11:02:54 +0000

    added characterisation tests

--- GildedRose/GildedRose/GildedRoseTest.cs
+++ GildedRose/GildedRose/GildedRoseTest.cs
@@ -6,12 +6,43 @@ namespace GildedRose
     public class GildedRoseTest
     {
         [Test]
-        public void foo()
+        public void ShouldReturnFooWhenItemNameIsFoo()
         {
             IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
             GildedRose app = new GildedRose(Items);
             app.UpdateQuality();
-            Assert.AreEqual("fixme", Items[0].Name);
+            Assert.AreEqual("foo", Items[0].Name);
+        }
+        
+        [Test]
+        public void ShouldReturnQualityOfZeroWhenStartingQualityIsZero()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(0, Items[0].Quality);
+        }
+        
+        [Test]
+        [TestCase(10)]
+        [TestCase(20)]
+        public void ShouldLowerQualityByTwo(int quality)
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = quality } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(quality - 2, Items[0].Quality);
+        }
+        
+        [Test]
+        [TestCase(10)]
+        [TestCase(20)]
+        public void ShouldIncreaseQualityByTwoWhenNameIsAgedBrie(int quality)
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 0, Quality = quality } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(quality + 2, Items[0].Quality);
         }
     }
 }
