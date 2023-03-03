b2f736432bf1b50a015902a45232820ad3c1dd5c
Ari Bakker <ari.bakker@asos.com>
2023-03-03 09:31:28 +0000

    initial commit

---
+++ GildedRose/GildedRose/ApprovalTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System.Reflection;
+using System.Text;
+
+namespace GildedRose
+{
+    [TestFixture]
+    public class ApprovalTest
+    {
+        [Test]
+        public void ThirtyDays()
+        {
+            var filePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "ThirtyDays.txt");
+
+            var lines = File.ReadAllText(filePath);
+
+            StringBuilder fakeoutput = new StringBuilder();
+            Console.SetOut(new StringWriter(fakeoutput));
+            Console.SetIn(new StringReader("\n"));
+
+            Program.Main(new string[] { });
+            String output = fakeoutput.ToString();
+
+            var outputLines = output.Split('\n');
+            var referenceLInes = lines.Split('\n');
+
+            for (var i = 0; i < Math.Min(lines.Length, outputLines.Length); i++)
+            {
+                Assert.AreEqual(referenceLInes[i], outputLines[i]);
+            }
+        }
+    }
+}
